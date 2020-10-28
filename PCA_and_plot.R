de707d00ee192e9c83fc8978f5a066cb181bb879
Arthur Zhu <67494243+wzhu221@users.noreply.github.com>
2020-10-28 14:43:31 +1300

    Create PCA_and_plot.R

---
+++ PCA_and_plot.R
@@ -0,0 +1,27 @@
+# PREPARATORY WORK BEFORE SERIOUS CODING #####
+# Before running this script, ensure the following packages are installed:
+# readxl, ggplot2, dplqr, devtools, ggbiplot, extrafont
+# Packages can be installed by directly using:
+# install.packages() in the console.
+# Load all necessary packages used in this script.\
+library('readxl') # for importing data from Excel files
+library('ggplot2')
+library('dplyr')
+library('ggbiplot')
+library('extrafont')
+library('factoextra')
+library('ggord') # dev version from fawda123/ggord")
+library('MASS')
+library('scales')
+#library('ElemStatLearn') # installed archived version
+library('class')
+library('caret')
+library('ggpubr') # dev version from kassambara/ggpubr
+library('cowplot') # dev version from wilkelab/cowplot
+library('Cairo') # requires XQuartz if Cairo package is run on OS X
+library('wesanderson')
+library('ggsci')
+library('ISLR')
+library('ggrepel')
+library('heatmaply')
+library('pheatmap')
